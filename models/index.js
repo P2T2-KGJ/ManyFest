ac7e5326ece271c5e8f0dbd97b7e517a3fffd140
Jesse Comeau <97984389+JesseComeau@users.noreply.github.com>
2022-05-03 19:05:34 -0500

    Updated model/index with relationships.

--- models/index.js
+++ models/index.js
@@ -1,3 +1,25 @@
 const Collection = require('./collections');
+const User = require('./users');
+const Item = require('./items');
 
-module.exports = { Collection }
+User.hasMany (Collection, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Collection.belongsTo (User, {
+    foreignKey: 'user_id',
+    unique: false,
+})
+
+Collection.hasMany (Item, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Item.belongsTo (Collection, {
+    foreignKey: 'user_id',
+    unique: false,
+})
+
+module.exports = { User, Item, Collection }
