996f62761088322349231f3e136cf2286b5ac26e
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-05-05 20:16:22 -0500

    added confirmUser

--- utils/auth.js
+++ utils/auth.js
@@ -7,4 +7,20 @@ module.exports = {
             next();
         }
     },
+    // takes session data & username and returns boolean on if it exists and matches session
+    confirmUser: async (sess, username) => {
+        const userData = await User.findOne(req.body, {
+            where: {
+                username,
+            },
+        });
+
+        if (!userData) {
+            return false;
+        }
+
+        const userInfo = userData.get({ plain: true });
+        // instead of sess.userId, use whatever you used to store the session userid in
+        return userInfo.id === sess.userId ? sess.userId : false;
+    },
 };
